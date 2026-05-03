71d2c039f0393d797b35ee773fe08157f5097b11
dk <kudin.dima@gmail.com>
2026-05-03 18:48:25 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Tai Yu | Japans & Chinees Restaurant',
+  description: 'Authentic taste. Personal service. Unforgettable evenings. Ervaar de verfijnde Japanse en Chinese keuken bij Tai Yu.',
+  openGraph: {
+    "title": "Tai Yu Restaurant",
+    "description": "Authentieke Japanse en Chinese gerechten.",
+    "siteName": "Tai Yu"
+  },
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
